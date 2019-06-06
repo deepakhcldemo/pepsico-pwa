87cfd626098e835128c097e01416f5328676bfd0
Madan Lalprajapati <madan.l@HCL.COM>
2019-06-06 14:58:59 +0530

    cat images

--- src/components/pages/Catalog/index.js
+++ src/components/pages/Catalog/index.js
@@ -292,197 +292,80 @@ class Catalog extends Component {
     categoryList = [
       {
         itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
+          title: "LEVEL 1 - EAST",
+          code: "15 METER AWAY",
+          imgsrc: "assets/images/cat1.png",
           desc: "test ",
           status: 80,
           quantity: 100,
           type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
+          thumbimgsrc: "assets/images/cat1.png",
+          imggallery: ["assets/images/cat1.png", "assets/images/cat1.png"]
         }
       },
       {
         itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
+          title: "LEVEL 2 - EAST",
+          code: "5 METER AWAY",
+          imgsrc: "assets/images/cat2.png",
           desc: "test ",
           status: 33,
           quantity: 100,
           type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
+          thumbimgsrc: "assets/images/cat2.png",
+          imggallery: ["assets/images/cat2.png", "assets/images/cat2.png"]
         }
       },
       {
         itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
+          title: "LEVEL 3 - WEST",
+          code: "10 METER AWAY",
+          imgsrc: "assets/images/cat3.png",
           desc: "test ",
           status: 80,
           quantity: 100,
           type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
+          thumbimgsrc: "assets/images/cat3.png",
+          imggallery: ["assets/images/cat3.png", "assets/images/cat3.png"]
         }
       },
       {
         itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
+          title: "LEVEL 4 - EAST",
+          code: "5 METER AWAY",
+          imgsrc: "assets/images/cat4.png",
           desc: "test ",
           status: 33,
           quantity: 100,
           type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
+          thumbimgsrc: "assets/images/cat4.png",
+          imggallery: ["assets/images/cat4.png", "assets/images/cat4.png"]
         }
       },
       {
         itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
+          title: "LEVEL 5 - WEST",
+          code: "1 METER AWAY",
+          imgsrc: "assets/images/cat5.png",
           desc: "test ",
           status: 80,
           quantity: 100,
           type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
+          thumbimgsrc: "assets/images/cat5.png",
+          imggallery: ["assets/images/cat5.png", "assets/images/cat5.png"]
         }
       },
       {
         itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
+          title: "LEVEL 6 - NORTH",
+          code: "0 METER AWAY",
+          imgsrc: "assets/images/cat6.png",
           desc: "test ",
           status: 33,
           quantity: 100,
           type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
-          desc: "test ",
-          status: 80,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
-          desc: "test ",
-          status: 33,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
-          desc: "test ",
-          status: 80,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
-          desc: "test ",
-          status: 33,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
-          desc: "test ",
-          status: 80,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
-          desc: "test ",
-          status: 33,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Classic",
-          code: "0122101",
-          imgsrc: "assets/images/img1.jpg",
-          desc: "test ",
-          status: 80,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img1.jpg",
-          imggallery: ["assets/images/img1.jpg", "assets/images/img1.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Barbeque Ham",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
-          desc: "test ",
-          status: 33,
-          quantity: 100,
-          type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
-        }
-      },
-      {
-        itemDetails: {
-          title: "Lays Barbeque Ham new",
-          code: "0122101",
-          imgsrc: "assets/images/img2.jpg",
-          desc: "test ",
-          status: 50,
-          quantity: 80,
-          type: "floor",
-          thumbimgsrc: "assets/images/img2.jpg",
-          imggallery: ["assets/images/img2.jpg", "assets/images/img2.jpg"]
+          thumbimgsrc: "assets/images/cat6.png",
+          imggallery: ["assets/images/cat6.png", "assets/images/cat6.png"]
         }
       }
     ];
